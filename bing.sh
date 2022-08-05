b685f438264171eae833b682b167354d81cabb7b
Kleber de Souza <kleber107@gmail.com>
2022-08-05 18:00:48 -0300

    Crontab

--- bing.sh
+++ bing.sh
@@ -3,4 +3,4 @@ nameImage="wallpaper$aleatorio.jpg"
 
 wget -O /tmp/$nameImage "http://www.bing.com/$(wget -q -O- https://binged.it/2ZButYc | sed -e 's/<[^>]*>//g' | cut -d / -f2 | cut -d \& -f1)"
 
-gsettings set org.gnome.desktop.background picture-uri-dark "file:///tmp/$nameImage"
+gsettings set org.gnome.desktop.background picture-uri "file:///tmp/$nameImage"
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 DIR="/home/kleber/Pictures/wallpapers/wallpapers/paisagens/"
 PIC=$(ls $DIR/* | shuf -n1)
 echo "file:///$PIC"
-gsettings set org.gnome.desktop.background picture-uri-dark "file:///$PIC"
+gsettings set org.gnome.desktop.background picture-uri "file:///$PIC"
