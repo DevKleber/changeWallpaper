67b497b3b721bd7453c684cbd95d9ba183458f64
Kleber de Souza <kleber107@gmail.com>
2022-08-07 12:00:27 -0300

    Crontab

--- bing.sh
+++ bing.sh
@@ -1,6 +1,11 @@
-aleatorio=$(($RANDOM % 1000))
+# aleatorio=$(($RANDOM%1000))
+aleatorio=""
 nameImage="wallpaper$aleatorio.jpg"
+
+# export DBUS_SESSION_BUS_ADDRESS environment variable
+
 
 wget -O /tmp/$nameImage "http://www.bing.com/$(wget -q -O- https://binged.it/2ZButYc | sed -e 's/<[^>]*>//g' | cut -d / -f2 | cut -d \& -f1)"
 
-gsettings set org.gnome.desktop.background picture-uri "file:///tmp/$nameImage"
+# gsettings set org.gnome.desktop.background picture-uri "file:///tmp/$nameImage"
+DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri "file:///tmp/$nameImage"
--- run.sh
+++ run.sh
@@ -2,5 +2,9 @@
 
 DIR="/home/kleber/Pictures/wallpapers/wallpapers/paisagens/"
 PIC=$(ls $DIR/* | shuf -n1)
-echo "file:///$PIC"
+# echo "file:///$PIC"
+
+PID=$(pgrep -o "cinnamon-sess|gnome-sess|mate-sess")
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
 gsettings set org.gnome.desktop.background picture-uri "file:///$PIC"
