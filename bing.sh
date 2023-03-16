89906d5a5a806ebac8e350ad75dbf89bfcafb43c
Kleber de Souza <kleber.souza@hiplatform.com>
2023-03-16 18:16:28 -0300

    Crontab

--- bing.sh
+++ bing.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # aleatorio=$(($RANDOM%1000))
 # aleatorio=""
 aleatorio=$(date +%d%m%Y)
@@ -10,7 +12,7 @@ export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/en
 wget -O /tmp/$nameImage "http://www.bing.com/$(wget -q -O- https://binged.it/2ZButYc | sed -e 's/<[^>]*>//g' | cut -d / -f2 | cut -d \& -f1)"
 
 # gsettings set org.gnome.desktop.background picture-uri "file:///tmp/$nameImage"
-gsettings set org.gnome.desktop.background picture-uri "file:///tmp/$nameImage"
+# gsettings set org.gnome.desktop.background picture-uri "file:///tmp/$nameImage"
 
 # Windows
 # Move file to windows folder. 
