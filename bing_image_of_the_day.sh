8a8610d146bac15639bbdfce165bf5f63310b190
Kleber de Souza <kleber.souza@hiplatform.com>
2023-06-24 16:00:13 -0300

    Crontab

--- bing_image_of_the_day.sh
+++ bing_image_of_the_day.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-curl -s "https://www.bing.com/?cc=br" > pagina.html
+caminho="/home/kleber/shellscripts/changeWallpaper"
+page="https://www.bing.com"
+
+curl -s "$page/?cc=br" > pagina.html
 
 description=$(grep -o 'Description":"[^"]*' pagina.html | sed 's/Description":"//')
 
-media_contents=$(grep -o 'MediaContents":\[{[^}]*}' pagina.html)
 media_contents=$(grep -o 'MediaContents":\[{[^}]*}' pagina.html | sed 's/MediaContents"://')
-
-echo "$media_contents}}]" > media_contents.json
-
-
-description=$(grep -o 'Description":"[^"]*' media_contents.json | sed 's/Description":"//')
-url=$(grep -o 'Url":"[^"]*' media_contents.json | sed 's/Url":"//')
-wallpaper=$(grep -o 'Wallpaper":"[^"]*' media_contents.json | sed 's/Wallpaper":"//')
 title=$(grep -o 'Title":"[^"]*' pagina.html | sed 's/Title":"//' | sed 's/Pesquise com a sua voz!$//')
 
-caminho="/home/kleber/shellscripts/changeWallpaper"
-nome_imagem=$(basename "$url")
+echo "$media_contents},\"titlePage\": \"$title\"}]" > media_contents.json
+
+description=$(grep -o 'Description":"[^"]*' media_contents.json | sed 's/Description":"//')
+urlImage=$(grep -o 'Url":"[^"]*' media_contents.json | sed 's/Url":"//')
+wallpaper=$(grep -o 'Wallpaper":"[^"]*' media_contents.json | sed 's/Wallpaper":"//')
+
+
+nome_imagem=$(basename "$urlImage")
 extensao=$(echo "$nome_imagem" | awk -F . '{print $NF}')
 
-wget "https://www.bing.com$url" -O "${caminho}/bing.$extensao"
+wget "$page$urlImage" -O "${caminho}/bing.$extensao"
 
 echo "Descrição: $description"
 echo "Wallpaper: $wallpaper"
-echo "Url: $url"
+echo "Url: $urlImage"
 echo "Título: $title"
