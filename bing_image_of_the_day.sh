4222637eaaf6552c1f1dd2932669363c796a1860
Kleber de Souza <kleber.souza@hiplatform.com>
2023-06-24 15:00:13 -0300

    Crontab

--- bing_image_of_the_day.sh
+++ bing_image_of_the_day.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
-
-# Faça o download da página
 curl -s "https://www.bing.com/?cc=br" > pagina.html
 
-# Extrair a descrição
 description=$(grep -o 'Description":"[^"]*' pagina.html | sed 's/Description":"//')
 
-# Extrair o URL do wallpaper
-wallpaper=$(grep -o 'Wallpaper":"[^"]*' pagina.html | sed 's/Wallpaper":"//')
-
-# Extrair o título
-title=$(grep -o 'Title":"[^"]*' pagina.html | sed 's/Title":"//' | sed 's/Pesquise com a sua voz!$//')
-
-# # Imprimir os resultados
-# echo "Descrição: $description"
-# echo "Wallpaper: $wallpaper"
-echo "Título: $title"
-
-# Extrair o objeto completo MediaContents
 media_contents=$(grep -o 'MediaContents":\[{[^}]*}' pagina.html)
+media_contents=$(grep -o 'MediaContents":\[{[^}]*}' pagina.html | sed 's/MediaContents"://')
+
 echo "$media_contents}}]" > media_contents.json
 
 
 description=$(grep -o 'Description":"[^"]*' media_contents.json | sed 's/Description":"//')
+url=$(grep -o 'Url":"[^"]*' media_contents.json | sed 's/Url":"//')
 wallpaper=$(grep -o 'Wallpaper":"[^"]*' media_contents.json | sed 's/Wallpaper":"//')
-title=$(grep -o 'Title":"[^"]*' media_contents.json | sed 's/Title":"//')
+title=$(grep -o 'Title":"[^"]*' pagina.html | sed 's/Title":"//' | sed 's/Pesquise com a sua voz!$//')
 
+caminho="/home/kleber/shellscripts/changeWallpaper"
+nome_imagem=$(basename "$url")
+extensao=$(echo "$nome_imagem" | awk -F . '{print $NF}')
+
+wget "https://www.bing.com$url" -O "${caminho}/bing.$extensao"
 
 echo "Descrição: $description"
 echo "Wallpaper: $wallpaper"
+echo "Url: $url"
 echo "Título: $title"
